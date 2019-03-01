73e5562001e00fa2a9c5e6ce26b8cacb0258743b
Jonas Johansson <jns.johansson@gmail.com>
2019-03-01 21:17:45 +0100

    Update

--- applications/review.js
+++ applications/review.js
@@ -1,21 +1,28 @@
 var links = document.querySelectorAll('a');
 for (let link of links){
-	let href = link.href;
-	if (href.includes('youtube')){
-		let id = href.split('v=')[1];
-		let iframe = document.createElement('iframe');
+	const url = link.href;
+	let iframe = document.createElement('iframe');
+	var id = false;
+	if (url.includes('youtu')){
+		id = youtube_parser(url);
 		iframe.src = `https://www.youtube.com/embed/${id}`;
+	} else if (url.includes('vimeo')){
+		id = vimeo_parser(url);
+		iframe.src = `https://player.vimeo.com/video/${id}`;
+	}
+	if (id){
 		link.parentNode.appendChild(iframe);
 		link.parentNode.removeChild(link);
-	} else if (href.includes('vimeo')){
-		let regExp = /https:\/\/(www\.)?vimeo.com\/(\d+)($|\/)/;
-		let match = href.match(regExp);
-		if (match){
-			let id = match[2];
-			let iframe = document.createElement('iframe');
-			iframe.src = `https://player.vimeo.com/video/${id}?color=ffffff&title=0&byline=0&portrait=0`;
-			link.parentNode.appendChild(iframe);
-			link.parentNode.removeChild(link);
-		}
 	}
+}
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+function vimeo_parser(url){
+	var regExp = /http(s)?:\/\/(www\.)?vimeo.com\/(\d+)(\/)?(#.*)?/
+	var match = url.match(regExp)
+	if (match)
+	    return match[3]
 }
