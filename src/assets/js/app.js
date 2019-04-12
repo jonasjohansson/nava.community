9a89de69a9bc30004c956cba04c2176dc0646dcc
Jonas Johansson <jns.johansson@gmail.com>
2019-04-13 00:52:50 +0200

    new type

---
+++ src/assets/js/app.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', () => {
+	// for (let a of document.querySelectorAll('a')) a.target = '_blank';
+});
--- src/eleventy.config.js
+++ src/eleventy.config.js
@@ -1,4 +1,36 @@
+// const CleanCSS = require('clean-css');
+// const UglifyJS = require('uglify-es');
+// const htmlmin = require('html-minifier');
+
 module.exports = eleventyConfig => {
+	// Minify CSS
+	// eleventyConfig.addFilter('cssmin', function(code) {
+	// 	return new CleanCSS({}).minify(code).styles;
+	// });
+
+	// // Minify JS
+	// eleventyConfig.addFilter('jsmin', function(code) {
+	// 	let minified = UglifyJS.minify(code);
+	// 	if (minified.error) {
+	// 		console.log('UglifyJS error: ', minified.error);
+	// 		return code;
+	// 	}
+	// 	return minified.code;
+	// });
+
+	// // Minify HTML output
+	// eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+	// 	if (outputPath.indexOf('.html') > -1) {
+	// 		let minified = htmlmin.minify(content, {
+	// 			useShortDoctype: true,
+	// 			removeComments: true,
+	// 			collapseWhitespace: true
+	// 		});
+	// 		return minified;
+	// 	}
+	// 	return content;
+	// });
+
 	eleventyConfig.addPassthroughCopy('assets');
 	return {
 		templateFormats: ['njk', 'md', 'html'],
