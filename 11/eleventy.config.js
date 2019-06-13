8abbad90d571a9d6b996d4eb7fd9a5cfea13e2fb
Jonas Johansson <jns.johansson@gmail.com>
2019-06-13 07:12:42 +0200

    test

--- 11/eleventy.config.js
+++ 11/eleventy.config.js
@@ -48,7 +48,7 @@ module.exports = eleventyConfig => {
 			input: '.',
 			includes: 'includes',
 			data: 'data',
-			output: 'docs'
+			output: '../'
 		},
 		markdownTemplateEngine: 'njk',
 		htmlTemplateEngine: 'njk',
