c588d1bf5d50b891fccfa8354528cb88422470ea
Jonas Johansson <jns.johansson@gmail.com>
2019-06-13 08:41:14 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -48,7 +48,7 @@ module.exports = eleventyConfig => {
 			input: '.',
 			includes: '11',
 			data: '11',
-			output: '/'
+			output: 'public'
 		},
 		markdownTemplateEngine: 'njk',
 		htmlTemplateEngine: 'njk',
