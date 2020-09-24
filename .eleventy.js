4d95d18a7f3417125451f1002418e2c947c43336
Jonas Johansson <jns.johansson@gmail.com>
2020-09-24 14:28:58 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -98,17 +98,12 @@ module.exports = function(eleventyConfig) {
     return content
   })
 
-  // eleventyConfig.addPassthroughCopy({ 'assets/fonts': 'assets/fonts' })
+  eleventyConfig.addPassthroughCopy({ 'assets/fonts': 'assets/fonts' })
+  eleventyConfig.addPassthroughCopy({ 'assets/video': 'assets/video' })
 
   return {
     templateFormats: ['css', 'json', 'md', 'njk', 'html', 'liquid'],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
     pathPrefix: '/',
-
     markdownTemplateEngine: 'liquid',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
